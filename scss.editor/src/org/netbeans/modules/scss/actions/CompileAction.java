d7a3539a3c038db1035e917ee423d5b9d63d9e81
jmarsault <jeremy.marsault@gmail.com>
2012-05-08 18:45:52 +0200

    Registered actions via annotations instead of layer.xml

--- scss.editor/src/org/netbeans/modules/scss/actions/CompileAction.java
+++ scss.editor/src/org/netbeans/modules/scss/actions/CompileAction.java
@@ -1,5 +1,9 @@
 package org.netbeans.modules.scss.actions;
 
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import org.netbeans.modules.scss.engine.Engine;
@@ -8,6 +12,13 @@ import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.util.Utilities;
 
+@ActionID(category = "Build",
+id = "org.netbeans.modules.scss.actions.CompileAction")
+@ActionRegistration(displayName = "#CTL_CompileAction")
+@ActionReferences({
+    @ActionReference(path = "Menu/BuildProject", position = 912, separatorBefore = 906, separatorAfter = 918),
+    @ActionReference(path = "Loaders/text/x-scss/Actions", position = 137, separatorBefore = 131, separatorAfter = 143)
+})
 public final class CompileAction implements ActionListener {
 
     public CompileAction(EditCookie context) {
