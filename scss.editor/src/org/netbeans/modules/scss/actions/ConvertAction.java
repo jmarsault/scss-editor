5f284c27e7a99d0a386f8af02a1735c304d3cea0
Jeremy MARSAULT <jeremy.marsault@gmail.com>
2012-08-15 13:02:21 +0200

    Registered actions via annotations instead of layer.xml

--- scss.editor/src/org/netbeans/modules/scss/actions/ConvertAction.java
+++ scss.editor/src/org/netbeans/modules/scss/actions/ConvertAction.java
@@ -1,22 +1,31 @@
 package org.netbeans.modules.scss.actions;
 
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import org.netbeans.modules.scss.engine.Engine;
-import org.openide.cookies.EditCookie;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.util.Utilities;
-import org.openide.filesystems.FileObject;
 
+@ActionID(category = "BpelNodes",
+id = "org.netbeans.modules.scss.actions.ConvertAction")
+@ActionRegistration(displayName = "#CTL_ConvertAction")
+@ActionReferences({
+    @ActionReference(path = "Loaders/text/x-css/Actions", position = 860, separatorBefore = 855, separatorAfter = 865)
+})
 public final class ConvertAction implements ActionListener {
 
-    public ConvertAction(EditCookie context) {
+    public ConvertAction(DataObject context) {
     }
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-	DataObject dobj = Utilities.actionsGlobalContext().lookup(DataObject.class);
-	FileObject fo = dobj.getPrimaryFile();
-	Engine.getDefault().convert(fo);
+        DataObject dobj = Utilities.actionsGlobalContext().lookup(DataObject.class);
+        FileObject fo = dobj.getPrimaryFile();
+        Engine.getDefault().convert(fo);
     }
 }
